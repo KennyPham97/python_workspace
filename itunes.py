ed0b170b933475ae3c8b2b129851eb4890061ac2
Kenny Pham <kpham@trinoor.local>
2024-06-06 13:58:46 -0400

    commenting on itunes.py

--- itunes.py
+++ itunes.py
@@ -10,3 +10,6 @@ response = requests.get("https://itunes.apple.com/search?entity=song&limit=100&t
 o = response.json()
 for result in o["results"]:
     print(result["trackName"])
+    
+    
+#this is a program that requests data from the itunes api and displays it in the terminal.
