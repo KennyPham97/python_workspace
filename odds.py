bfa6093f5c025d17b0261d05ca5298e50d4fb98b
Kenny Pham <kenny.pham@trinoor.com>
2024-07-27 15:51:03 -0400

    new commit

--- odds.py
+++ odds.py
@@ -2,25 +2,20 @@ import sys
 
 def main():
     if len(sys.argv) != 3:
-        print("Console input must have 2 arguments, an under and over value.")
-        sys.exit()
-    
+        sys.exit("Console input must have 2 more arguments, an under and over value.")
+        
     while True:
         try:
             under = abs(int(sys.argv[1]))
             over = abs(int(sys.argv[2]))
             break
         except ValueError:
-            print("Invalid input type.")
-            sys.exit()
+            sys.exit("Invalid input type.")
     calculate_odds(under, over)
-    
-    
     
 def calculate_odds(under, over):
     under_odds = under/(under+100)*100
     over_odds = over/(over+100)*100
     print(f"{round(under_odds, 2)}%, {round(over_odds, 2)}%")  
     
-
 main()
